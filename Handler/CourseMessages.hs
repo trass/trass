be1540d009a60c42a21ec7f011072bededd94a7f
Nickolay Kudasov <nickolay.kudasov@gmail.com>
2014-11-13 01:00:16 +0300

    display message author name

--- Handler/CourseMessages.hs
+++ Handler/CourseMessages.hs
@@ -1,6 +1,9 @@
 module Handler.CourseMessages where
 
 import Import
+import Control.Arrow ((&&&))
+import Data.Maybe
+import qualified Data.Map as Map
 import qualified Data.Text as Text
 import Data.Time
 import Yesod.Auth
@@ -9,10 +12,8 @@ import Utils
 getCourseMessagesR :: Text -> Handler Html
 getCourseMessagesR courseI = do
   authId <- requireAuthId
-  messages <- runDB $ selectList [MessageCourseIdent ==. courseI, MessageStudent ==. authId] [Desc MessageDateTime]
   now <- liftIO getCurrentTime
-  defaultLayout $ do
-    $(widgetFile "student/messages")
+  displayMessages courseI authId now
 
 postCourseMessagesR :: Text -> Handler Html
 postCourseMessagesR courseI = do
@@ -27,6 +28,15 @@ postCourseMessagesR courseI = do
     Nothing -> invalidArgs ["message"]
     _ -> return () -- silently ignore empty messages
 
-  messages <- runDB $ selectList [MessageCourseIdent ==. courseI, MessageStudent ==. authId] [Desc MessageDateTime]
+  displayMessages courseI authId now
+
+displayMessages :: Text -> UserId -> UTCTime -> Handler Html
+displayMessages courseI authId now = do
+  messages    <- runDB $ selectList [MessageCourseIdent ==. courseI, MessageStudent ==. authId] [Desc MessageDateTime]
+  authors     <- runDB $ selectList [ProfileUser <-. map (messageAuthor . entityVal) messages] []
+  let
+    authors'    = filter (isJust . profileName) $ map entityVal authors
+    authorsMap  = Map.fromList $ map (profileUser &&& (fromJust . profileName)) authors'
+
   defaultLayout $ do
     $(widgetFile "student/messages")
