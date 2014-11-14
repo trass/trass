383bfbd6ce3f1b2bde23e61331cd282bfe2d7bc9
Nickolay Kudasov <nickolay.kudasov@gmail.com>
2014-11-15 00:12:42 +0300

    assignment page

--- Handler/CourseAssignment.hs
+++ Handler/CourseAssignment.hs
@@ -1,9 +1,44 @@
 module Handler.CourseAssignment where
 
 import Import
+import Yesod.Auth
+import Control.Monad
+import qualified Data.List as List
+import Handler.CourseSection
+import UserRole
 
 getCourseAssignmentR :: Text -> [Text] -> Handler Html
-getCourseAssignmentR = error "Not yet implemented: getCourseAssignmentR"
+getCourseAssignmentR cid path@(_:_:_) = do
+  let
+    sids = List.init path
+    aid  = List.head $ reverse path
+
+  Entity courseId  course  <- runDB $ getBy404 $ UniqueCourse cid
+  Entity sectionId section <- runDB $ getBy404 $ UniqueSection courseId (mkSectionIdent sids)
+
+  mauthId <- maybeAuthId
+  userRole <- maybe (return RoleStudent) (getUserRole cid) mauthId
+
+  when (sectionLocked section && not (isTeacher userRole)) $ do
+    notFound
+
+  Entity assignmentId assignment <- runDB $ getBy404 $ UniqueAssignment sectionId aid
+
+  let sectionPaths = List.tail $ List.inits sids
+  crumbs <- mapM (getSectionTitle courseId) sectionPaths
+
+  let
+    isCourseOwner = mauthId == Just (courseOwner course)
+    isCoursePreview   = True
+    isCourseGraphs    = False
+    isCourseSettings  = False
+    courseHeader = $(widgetFile "course/header")
+
+  setCourseIdent cid
+
+  defaultLayout $ do
+    $(widgetFile "course/assignment")
+getCourseAssignmentR _ _ = notFound
 
 postCourseAssignmentR :: Text -> [Text] -> Handler Html
 postCourseAssignmentR = error "Not yet implemented: postCourseAssignmentR"
