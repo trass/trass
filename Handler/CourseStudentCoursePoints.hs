ddeab16abc5eb91ea97251ce1e9e995faf088fff
Nickolay Kudasov <nickolay.kudasov@gmail.com>
2014-11-16 05:02:17 +0300

    student course points page

--- Handler/CourseStudentCoursePoints.hs
+++ Handler/CourseStudentCoursePoints.hs
@@ -1,9 +1,66 @@
 module Handler.CourseStudentCoursePoints where
 
 import Import
+import Yesod.Auth
+import Yesod.Core.Dispatch
+import Control.Monad
+
+import UserRole
+import SubmissionStatus
+
+import Data.Time
+import qualified Data.Text as Text
+import Text.Read (readMaybe)
+
+import Handler.CourseStudent
+import Handler.CourseAssignment (assignmentR)
+import Handler.CourseSection (sectionR)
+
+import Utils
+import UtilsDB
 
 getCourseStudentCoursePointsR :: Text -> UserId -> Handler Html
-getCourseStudentCoursePointsR = error "Not yet implemented: getCourseStudentCoursePointsR"
+getCourseStudentCoursePointsR cname uid = do
+  Entity courseId _ <- runDB $ getBy404 $ UniqueCourse cname
+  Entity _ profile  <- runDB $ getBy404 $ UniqueProfile uid
+
+  studentRole <- getUserRole cname uid
+  when (not $ isStudent studentRole) $ notFound
+
+  mauthId <- maybeAuthId
+  userRole <- maybe (return RoleStudent) (getUserRole cname) mauthId
+
+  let isOtherStudent = isStudent userRole && Just uid /= mauthId
+  when isOtherStudent $ do
+    notFound
+
+  coursePoints <- runDB $ getStudentCoursePointsSum courseId uid
+
+  pageNoStr <- lookupGetParam "page"
+
+  totalEvents <- runDB $ getStudentCoursePointsCount courseId uid
+  let totalPages = ceiling (fromIntegral totalEvents / fromIntegral perPage)
+
+  pageNo <-
+    case pageNoStr >>= readMaybe . Text.unpack of
+      Nothing -> return 1
+      Just n | 1 <= n && n <= totalPages -> return n
+      _ -> notFound
+
+  coursePointsEvent <- runDB $ getStudentCoursePoints perPage pageNo courseId uid
+
+  now <- liftIO getCurrentTime
+  let tab = $(widgetFile "course/student/events")
+
+  defaultLayout $ do
+    $(widgetFile "course/student")
+  where
+    nav :: Widget
+    nav = $(widgetFile "course/student/nav")
+    tabName = "points"
+    perPage = 20
+    pageR n = (CourseStudentCoursePointsR cname uid, [("page", Text.pack $ show $ n)])
 
 postCourseStudentCoursePointsR :: Text -> UserId -> Handler Html
 postCourseStudentCoursePointsR = error "Not yet implemented: postCourseStudentCoursePointsR"
+
